290ae2a47071c44b13becd48f27489e8c0b8f9ad
Matheus <m6.vargas@gmail.com>
2020-03-10 23:42:44 -0300

    Changes to make mouse control possible

--- src/Player.gd
+++ src/Player.gd
@@ -1,27 +1,3 @@
-#extends KinematicBody2D
-#
-#export (int) var playerSpeed
-#var motion = Vector2(0, 0)
-#var target = Vector2(0, 0)
-#
-#func _ready():
-#	set_process(true)
-#
-#func _process(delta):
-#	if Input.is_action_pressed("ui_up"):
-#		motion.y -= playerSpeed * delta
-#	elif Input.is_action_pressed("ui_down"):
-#		motion.y += playerSpeed * delta
-#	elif Input.is_action_pressed("ui_left"):
-#		motion.x -= playerSpeed * delta
-#	elif Input.is_action_pressed("ui_right"):
-#		motion.x += playerSpeed * delta
-#	else:
-#		motion.x = 0
-#		motion.y = 0
-#
-#	motion = move_and_slide(motion)
-
 extends KinematicBody2D
 
 export (int) var speed = 200
